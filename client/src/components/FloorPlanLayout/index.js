86f7356e14d339852c7a0e3e82d9a788887bfe9f
Ru <rudikraeher@gmail.com>
2021-01-07 16:40:12 -0800

    fixing indentations - ready for component insertion

--- client/src/components/FloorPlanLayout/index.js
+++ client/src/components/FloorPlanLayout/index.js
@@ -4,21 +4,33 @@ import "./index.css";
 function FloorPlanLayout() {
     return (
         <div className="wrapper">
-            <header className="main-head">The header</header>
+
+            <header className="main-head">
+                The header component goes here
+            </header>
+
             <nav className="main-nav">
                 <ul>
-                    <li><a href="">Nav 1</a></li>
-                    <li><a href="">Nav 2</a></li>
-                    <li><a href="">Nav 3</a></li>
+                    <li>Collapsible Side Nav Component</li>
                 </ul>
             </nav>
+
             <article className="content">
-                <h1>Main article area</h1>
-                <p>In this layout, we display the areas in source order for any screen less that 500 pixels wide. We go to a two column layout, and then to a three column layout by redefining the grid, and the placement of items on the grid.</p>
+                <h1>Main floor plan area</h1>
+                <p>This is where the full floor plan goes</p>
             </article>
-            <aside className="side">Sidebar Search</aside>
-            <div className="ad">Sidebar Bin</div>
-            <footer className="main-footer">The footer</footer>
+
+            <aside className="side">
+                Sidebar Search
+            </aside>
+
+            <div className="ad">
+                Sidebar Bin
+            </div>
+
+            <footer className="main-footer">
+                The footer
+            </footer>
         </div>
     );
 }
