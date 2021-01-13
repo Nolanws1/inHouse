d8532795e9ba3ccf810304afc44ff4db6da41d0a
Nolanws1 <Nolanws@gmail.com>
2021-01-12 20:34:06 -0800

    building dash, no need to merge this.

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -2,16 +2,29 @@ import React from 'react'
 // import { Container } from '../../components/Grid/index';
 import Navbar from '../../components/NavTabs/index';
 import { BrowserRouter as Router } from "react-router-dom";
-
-import FloorPlanLayout from "../FloorPlanLayout";
+import "./style.css";
+import { ListGroupItem } from 'react-bootstrap';
+import FloorPlanImg from '../../images/floorplans/floorplan.png';
 
 const Dashboard = () => {
     return (
-        // <Container className="-fluid">
-        <div>
-            <FloorPlanLayout />
-        </div>
-        // </Container>
+    <div className="grid-container">
+            <div className="header">Header</div>
+            
+            
+            <div className="dash-col-1">
+                <section class="welcome">
+                <h1>Welcome, _______!</h1>
+                <h2>00:00 |  Jan, 3, 2021</h2>
+                </section>
+                <img src={FloorPlanImg} alt="floorplan button" className="floorplan" />
+            </div>
+            
+            
+            
+            
+            <div className="table">Table</div>
+    </div>
     )
 }
 
