84f21b68f3658b73e2deafcc94d595ee9351cb6c
Nolanws1 <Nolanws@gmail.com>
2021-01-14 13:32:25 -0800

    creating table header componenet

---
+++ client/src/components/Table/TableHeading/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+function TableHeading({ handleClick }) {
+    return (
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col"
+                    // onClick={handleClick}
+                    data-value="first">
+                    Item
+                </th>
+                <th scope="col"
+                    // onClick={handleClick}
+                    data-value="last">
+                </th>
+                <th scope="col">
+                    Qty.
+                </th>
+            </tr>
+        </thead>
+    )
+}
+
+export default TableHeading;
--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import './style.css'
+import TableHeading from './TableHeading';
 
 const Table = () => {
     return (
   <table className="table table-dark color">
-  <thead className="color">
-    <tr>
-      <th scope="col">Item #</th>
-      <th scope="col">Item</th>
-      <th scope="col">Category</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1908-S</td>
-      <td>LONDON Swivel Chair blue</td>
-      <td>Chair</td>
-    </tr>
-  </tbody>
+  <TableHeading />
+ 
 </table>
     )
 }
