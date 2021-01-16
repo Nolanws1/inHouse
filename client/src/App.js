d8606364964446d6c84d0fdf4c2bd011a0f28bfa
Cody Mullen <mullen.cody@yahoo.com>
2021-01-15 21:57:43 -0800

    removing commented out code for user auth

--- client/src/App.js
+++ client/src/App.js
@@ -38,28 +38,3 @@ function App() {
 
 export default App;
 
-
-//Below is the code for user authentication --------------------------------------
-
-// import React from 'react';
-// import LoginButton from './components/LoginButton/LoginButton';
-// import LogoutButton from './components/LogoutButton/LogoutButton';
-// import Profile from './components/Profile/Profile';
-// import { useAuth0 } from '@auth0/auth0-react'; 
-
-
-// function App() {
-//   const { isLoading } = useAuth0();
-
-//   if (isLoading) return <div> Loading... </div>
-
-//   return (
-//     <>
-//     <LoginButton />
-//     <LogoutButton />
-//     <Profile />
-//     </>
-//   );
-// }
-
-// export default App;
