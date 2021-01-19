c68f7186d7af95df1ba699cc7db63cd242abcbc4
cmatsuta <cmatsuta@jbs.com>
2021-01-18 20:32:44 -0800

    moved updateinventoryform in formtab

--- client/src/components/FormTab/index.js
+++ client/src/components/FormTab/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
 import CreateItemForm from '../CreateItemForm';
-// import UpdateInventoryForm from '../UpdateInventoryForm';
+import UpdateInventoryForm from '../UpdateInventoryForm';
 import './style.css';
 
 const FormTab = () => {
@@ -9,9 +9,9 @@ const FormTab = () => {
         <Tabs defaultActiveKey="Insert" transition={false} id="noanim-tab-example" className="tabs-background">
         <Tab eventKey="Insert" title="Insert" className="single-tab">
           <CreateItemForm />
-          {/* <UpdateInventoryForm /> */}
         </Tab>
         <Tab eventKey="Update" title="Update">
+          <UpdateInventoryForm />
         </Tab>
       </Tabs> 
     ) 
