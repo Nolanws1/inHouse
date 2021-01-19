1e5b805918866499950676e59ebed8a491a6533f
Ru <rudikraeher@gmail.com>
2021-01-19 13:33:33 -0800

    mouse hover effects on fpdiag table

--- client/src/components/FloorTable/FloorRow/index.js
+++ client/src/components/FloorTable/FloorRow/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
+function handleOver(e) {
+    e.target.style.background = "#6C757D";
+}
+function handleLeave(e) {
+    e.target.style.background = "#343A40";
+}
+
 function FloorRow({ item, handleClick, index }) {
     return (
         <tr key={index}>
             <td onClick={handleClick}
+                onMouseOver={handleOver}
+                onMouseLeave={handleLeave}
                 data-number={item.itemNumber}
                 data-name={item.itemName}>
                 {item != undefined && item.itemNumber}
             </td>
             <td onClick={handleClick}
+                onMouseOver={handleOver}
+                onMouseLeave={handleLeave}
                 data-number={item.itemNumber}
                 data-name={item.itemName}>
                 {item != undefined && item.itemName}
