39b15709c670af023f1273c9d320665dab520c24
Nolan Sanchez <nolansanchez@nolans-imac.lan>
2021-01-18 20:11:09 -0800

    working navtabs. starting tabs for forms. changing sidebar icons. repsonive css changes. more styling

--- client/src/pages/Forms/index.js
+++ client/src/pages/Forms/index.js
@@ -3,6 +3,7 @@ import CreateItemForm from "../../components/CreateItemForm"
 import FormTable from '../../components/FormTable';
 import './style.css';
 import { useAuth0 } from '@auth0/auth0-react';
+import FormTab from '../../components/FormTab'
 
 const Forms = () => {
   const { isAuthenticated } = useAuth0();
@@ -13,8 +14,7 @@ const Forms = () => {
         <div className="header">Header</div>
 
         <div className="form">
-          <h1>Form</h1>
-          <CreateItemForm />
+          <FormTab />
         </div>
 
         <div className="items">
